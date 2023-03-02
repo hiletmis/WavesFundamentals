b48047e2440b9958bcab84e720218b10ad3f71a6
hiletmis <hayrettin@ennowallet.com>
2023-03-02 14:28:11 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WavesFundamentals
+
+A description of this package.
---
+++ Tests/WavesFundamentalsTests/WavesFundamentalsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import WavesFundamentals
+
+final class WavesFundamentalsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(WavesFundamentals().text, "Hello, World!")
+    }
+}
